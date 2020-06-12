eab14371249ec3f1660487d9e379a513de2f0af4
Bruno Soares <bruno@stoodi.com.br>
2020-06-11 22:39:32 -0300

    feat:improvements on create tables

--- database.py
+++ database.py
@@ -1,18 +1,35 @@
+# import os
+# from sqlalchemy import create_engine
+# from sqlalchemy.orm import scoped_session, sessionmaker
+# from sqlalchemy.ext.declarative import declarative_base
+
+# engine = create_engine(os.getenv("DATABASE_URL"))
+# db = scoped_session(sessionmaker(autocommit=False,
+#                                          autoflush=False,
+#                                          bind=engine))
+# Base = declarative_base()
+# Base.query = db.query_property()
+
+# def init_db():
+#     # import all modules here that might define models so that
+#     # they will be registered properly on the metadata.  Otherwise
+#     # you will have to import them first before calling init_db()
+#     import models
+#     Base.metadata.create_all(bind=engine)
+
 import os
-from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine(os.getenv("DATABASE_URL"))
-db = scoped_session(sessionmaker(autocommit=False,
-                                         autoflush=False,
-                                         bind=engine))
-Base = declarative_base()
-Base.query = db.query_property()
+from flask import Flask, render_template, request
+from models import *
 
-def init_db():
-    # import all modules here that might define models so that
-    # they will be registered properly on the metadata.  Otherwise
-    # you will have to import them first before calling init_db()
-    import models
-    Base.metadata.create_all(bind=engine)
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db.init_app(app)
+
+def main():
+    db.create_all()
+
+if __name__ == "__main__":
+    with app.app_context():
+        main()
